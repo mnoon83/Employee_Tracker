8f3675b56cce231d723b67a1d785828164e087e8
Michael Noon <m.noon83@yahoo.com>
2021-10-12 01:09:05 -0700

    added seed file

---
+++ seed.sql
@@ -0,0 +1,7 @@
+USE users_db;
+INSERT INTO dept (id, name)
+VALUES ()
+INSERT INTO job (id, title, salary, dept_id)
+VALUES()
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES()
