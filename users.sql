cc0030dff9bc65d355d6e5a19f9d8f39f31145f5
Michael Noon <m.noon83@yahoo.com>
2021-10-12 00:57:48 -0700

    code

--- users.sql
+++ users.sql
@@ -22,3 +22,11 @@ CREATE TABLE employee (
     role_id INT,
     manager_id INT,
 );
+
+USE users_db;
+INSERT INTO dept (id, name)
+VALUES ()
+INSERT INTO job (id, title, salary, dept_id)
+VALUES()
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES()
